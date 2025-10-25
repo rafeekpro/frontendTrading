bbcc84c2989210a7f6458f9a0f4dabecc456e3bf
Rafal Lagowski <rla@Rafals-MacBook-Pro.local>
2025-10-26 01:30:00 +0200

    Issue #28 Stream C: Add /dashboard route to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from './components/ThemeProvider';
 import { Layout } from './components/layout';
 import ComponentShowcase from './pages/ComponentShowcase';
+import { Dashboard } from './pages/Dashboard';
 import { queryClient } from './lib/query-client';
 
 function App() {
@@ -12,7 +13,10 @@ function App() {
       <BrowserRouter>
         <ThemeProvider>
           <Layout>
-            <ComponentShowcase />
+            <Routes>
+              <Route path="/" element={<ComponentShowcase />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
           </Layout>
         </ThemeProvider>
       </BrowserRouter>
