e432966e1047bd389bd8e44f81648c21dc72048d
Rafal Lagowski <rla@Rafals-MacBook-Pro.local>
2025-10-25 23:32:40 +0200

    refactor: add barrel exports and integrate Layout into App (REFACTOR phase) #13
    - Add barrel export (index.ts) for layout components
    - Update App.tsx to use Layout with BrowserRouter
    - Layout now wraps entire application
    - All 274 tests passing
    
    ♻️ REFACTOR phase of TDD cycle complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from './components/ThemeProvider';
+import { Layout } from './components/layout';
 import ComponentShowcase from './pages/ComponentShowcase';
 
 function App() {
   return (
-    <ThemeProvider>
-      <ComponentShowcase />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider>
+        <Layout>
+          <ComponentShowcase />
+        </Layout>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
