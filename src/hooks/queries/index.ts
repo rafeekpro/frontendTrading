82b643d48b018efdaf2d70f940e2577a5c4177c0
Rafal Lagowski <rla@Rafals-MacBook-Pro.local>
2025-10-26 01:02:45 +0200

    Issue #27 Stream B: Add barrel export for query hooks

---
+++ src/hooks/queries/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Query Hooks Barrel Export
+ * Centralized export for all TanStack Query hooks
+ */
+
+export { useInstruments } from './use-instruments';
+export { useInstrument } from './use-instrument';
+export { useTrades } from './use-trades';
+export { useMarketData } from './use-market-data';
